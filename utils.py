Fixed errors in Jacobian calculation, previous implentation assumed that there is one random effect for each population coeffcients. The J calculations did not need to reference the random effects.

utils.py
@@ -211,7 +211,7 @@ def estimate_fprime_jac(pop_coeffs,thetas, theta_data, n_random_effects, y, mode
             model_coeffs_local = model_obj._generate_pk_model_coeff_vectorized(pop_coeffs_inner, thetas, theta_data)
             return model_obj._solve_ivp(model_coeffs_local, parallel=False)
     J_afp = approx_fprime(pop_coeffs.values.flatten(), wrapped_solve_ivp, epsilon=1e-6)
-    J_afp = J_afp.reshape(len(y), n_random_effects)
+    J_afp = J_afp.reshape(len(y), len(pop_coeffs.columns))
     return J_afp
 
 def estimate_cdiff_jac(pop_coeffs,
@@ -223,9 +223,9 @@ def estimate_cdiff_jac(pop_coeffs,
     plus_pop_coeffs = pop_coeffs.copy()
     minus_pop_coeffs = pop_coeffs.copy()
     epsilon = 1e-6
-    J_cd = np.zeros((len(y), n_random_effects))
-    for omega_idx, omega_c in enumerate(omegas_names):
-        c = omega_c[0]
+    J_cd = np.zeros((len(y), len(pop_coeffs.columns)))
+    for pop_coeff_idx, pop_coeff_i in enumerate(pop_coeffs.columns):
+        c = pop_coeff_i[0]
         plus_pop_coeffs[c] = plus_pop_coeffs[c] + epsilon
         plus_model_coeffs = model_obj._generate_pk_model_coeff_vectorized(plus_pop_coeffs,
                                                                         thetas, theta_data)
@@ -236,7 +236,7 @@ def estimate_cdiff_jac(pop_coeffs,
                                                                         thetas, theta_data)
         minus_preds = model_obj._solve_ivp(minus_model_coeffs, parallel = False, )
 
-        J_cd[:, omega_idx] = (plus_preds - minus_preds) / (2*epsilon) #the central difference
+        J_cd[:, pop_coeff_idx] = (plus_preds - minus_preds) / (2*epsilon) #the central difference
     return J_cd
 
 def estimate_jacobian(pop_coeffs:pd.DataFrame,