35c505ec4d8b177b7e3b831ecb2b674548972525
Kelley Brady <kelleyjbrady@protonmail.com>
2025-04-15 16:42:56 +0000

    Updated fo foce debug file to better organize results, perform additional tests.

--- debug_FO_FOCE.py
+++ debug_FO_FOCE.py
@@ -43,9 +43,40 @@ if apply_unit_conversion:
         scale_df['dose_scale'] = 1.0
     else:
         scale_df['DV_scale']= scale_df['DV_ng/L'].copy() / 20000000
+        #scale_df['DV_scale'] = np.log()
+        scale_df['DV_scale_alt'] = scale_df['DV_ng/L'] / np.exp(np.mean(np.log(scale_df['DV_ng/L'])))
         scale_df['dose_scale'] = scale_df['dose_ng'].copy()
+        scale_df['DV_ln'] = np.log(scale_df['DV_ng/L'])
 
-
+res_df = pd.DataFrame()
+#%%
+no_me_mod_fo =  CompartmentalModel(
+          ode_t0_cols=[ODEInitVals('DV')],
+          conc_at_time_col = 'DV',
+          population_coeff=[PopulationCoeffcient('cl', 18, subject_level_intercept=False,
+                                                 #subject_level_intercept_sd_init_val = 0.2, 
+                                                 #subject_level_intercept_sd_lower_bound=1e-6
+                                                 ),
+                            PopulationCoeffcient('vd', 30, ),
+                         ],
+          dep_vars= None, 
+                                   no_me_loss_function=sum_of_squares_loss, 
+                                   no_me_loss_needs_sigma=False,
+                                   #optimizer_tol=.001, 
+                                   pk_model_function=first_order_one_compartment_model2, 
+                                   model_error_sigma=PopulationCoeffcient('sigma'
+                                                                          ,log_transform_init_val=False
+                                                                          , optimization_init_val=.5
+                                                                          ,optimization_lower_bound=0.000001
+                                                                          ,optimization_upper_bound=5
+                                                                          ),
+                                   #ode_solver_method='BDF'
+                                  # optimizer_tol = .1
+                                  #minimize_method = 'COBYQA'
+                                   )
+res_df['DV'] = scale_df['DV']
+no_me_mod_fo = no_me_mod_fo.fit2(scale_df,checkpoint_filename=f'mod_abs_test_me_fo{now_str}.jb', n_iters_per_checkpoint=1, parallel=False, parallel_n_jobs=4)
+res_df['no_me_fo_sse'] = no_me_mod_fo.predict2(scale_df)
 # %%
 me_mod_fo =  CompartmentalModel(
           ode_t0_cols=[ODEInitVals('DV')],
@@ -53,6 +84,69 @@ me_mod_fo =  CompartmentalModel(
           population_coeff=[PopulationCoeffcient('cl', 18, subject_level_intercept=True,
                                                  subject_level_intercept_sd_init_val = 0.2, 
                                                  subject_level_intercept_sd_lower_bound=1e-6
+                                                 ),
+                            PopulationCoeffcient('vd', 30, ),
+                         ],
+          dep_vars= None, 
+                                   no_me_loss_function=sum_of_squares_loss, 
+                                   no_me_loss_needs_sigma=False,
+                                   #optimizer_tol=.001, 
+                                   pk_model_function=first_order_one_compartment_model2, 
+                                   model_error_sigma=PopulationCoeffcient('sigma'
+                                                                          ,log_transform_init_val=False
+                                                                          , optimization_init_val=.5
+                                                                          ,optimization_lower_bound=0.000001
+                                                                          ,optimization_upper_bound=5
+                                                                          ),
+                                   #ode_solver_method='BDF'
+                                  # optimizer_tol = .1
+                                  #minimize_method = 'COBYQA'
+                                   )
+
+
+
+me_mod_fo = me_mod_fo.fit2(scale_df,checkpoint_filename=f'mod_abs_test_me_fo{now_str}.jb', n_iters_per_checkpoint=1, parallel=False, parallel_n_jobs=4)
+res_df['me_fo_preds'] = me_mod_fo.predict2(scale_df)
+#%%
+me_mod_fo_ln =  CompartmentalModel(
+          ode_t0_cols=[ODEInitVals('DV_ln')],
+          conc_at_time_col = 'DV_ln',
+          population_coeff=[PopulationCoeffcient('cl', 18, subject_level_intercept=True,
+                                                 subject_level_intercept_sd_init_val = 0.2, 
+                                                 subject_level_intercept_sd_lower_bound=1e-6
+                                                 ),
+                            PopulationCoeffcient('vd', 30, ),
+                         ],
+          dep_vars= None, 
+                                   no_me_loss_function=sum_of_squares_loss, 
+                                   no_me_loss_needs_sigma=False,
+                                   #optimizer_tol=.001, 
+                                   pk_model_function=first_order_one_compartment_model2, 
+                                   model_error_sigma=PopulationCoeffcient('sigma'
+                                                                          ,log_transform_init_val=False
+                                                                          , optimization_init_val=.5
+                                                                          ,optimization_lower_bound=0.000001
+                                                                          ,optimization_upper_bound=5
+                                                                          ),
+                                   #ode_solver_method='BDF'
+                                  # optimizer_tol = .1
+                                  minimize_method = 'COBYQA'
+                                   )
+
+
+
+me_mod_fo_ln = me_mod_fo_ln.fit2(scale_df,checkpoint_filename=f'mod_abs_test_me_fo{now_str}.jb', n_iters_per_checkpoint=1, parallel=False, parallel_n_jobs=4)
+res_df['me_fo_preds_ln'] = me_mod_fo_ln.predict2(scale_df)
+
+
+#%%
+me_mod_fo_scale =  CompartmentalModel(
+          ode_t0_cols=[ODEInitVals('DV_scale')],
+          conc_at_time_col = 'DV_scale',
+          population_coeff=[PopulationCoeffcient('cl', 18, subject_level_intercept=True,
+                                                 subject_level_intercept_sd_init_val = 0.2, 
+                                                 subject_level_intercept_sd_lower_bound=1e-6, 
+                                                 #subject_level_intercept_sd_upper_bound=1
                                                  ),
                             PopulationCoeffcient('vd', 30, ),
                          ],
@@ -68,14 +162,16 @@ me_mod_fo =  CompartmentalModel(
                                                                           ,optimization_upper_bound=5
                                                                           ),
                                    #ode_solver_method='BDF'
+                                   minimize_method = 'COBYQA'
                                    )
 
 
 
-me_mod_fo = me_mod_fo.fit2(scale_df,checkpoint_filename=f'mod_abs_test_me_fo{now_str}.jb', n_iters_per_checkpoint=1, parallel=False, parallel_n_jobs=4)
-scale_df['me_fo_preds'] = me_mod_fo.predict2(scale_df)
-#%%
+#me_mod_fo_scale = me_mod_fo_scale.fit2(scale_df,checkpoint_filename=f'mod_abs_test_me_fo{now_str}.jb', n_iters_per_checkpoint=1, parallel=False, parallel_n_jobs=4)
+#scale_df['me_fo_preds_alt_scale'] = me_mod_fo_scale.predict2(scale_df)
 
+
+#%%
 b_i_apprx_df = pd.DataFrame( dtype = pd.Float64Dtype())
 b_i_apprx_df['b_i_fo_cl'] = me_mod_fo.b_i_approx[('cl', 'omega2_cl')].to_numpy()
 b_i_apprx_df['SUBJID'] = scale_df['SUBJID'].drop_duplicates().values
@@ -83,9 +179,9 @@ scale_df = (scale_df.merge(b_i_apprx_df, how = 'left', on = 'SUBJID')
             if 'b_i_fo_cl' not in scale_df.columns else scale_df.copy())
 
 me_mod_foce =  CompartmentalModel(
-          ode_t0_cols=[ODEInitVals('DV_scale')],
-          conc_at_time_col = 'DV_scale',
-          population_coeff=[PopulationCoeffcient('cl', 25, subject_level_intercept=True,
+          ode_t0_cols=[ODEInitVals('DV')],
+          conc_at_time_col = 'DV',
+          population_coeff=[PopulationCoeffcient('cl', 20, subject_level_intercept=True,
                                                  optimization_lower_bound = np.log(15), 
                                                  optimization_upper_bound = np.log(40),
                                                  subject_level_intercept_sd_init_val = 0.38, 
@@ -93,8 +189,8 @@ me_mod_foce =  CompartmentalModel(
                                                  subject_level_intercept_sd_upper_bound = 2,
                                                  subject_level_intercept_init_vals_column_name='b_i_fo_cl',
                                                  ),
-                            PopulationCoeffcient('vd', 80
-                                                 , optimization_lower_bound = np.log(70)
+                            PopulationCoeffcient('vd', 68
+                                                 , optimization_lower_bound = np.log(60)
                                                  , optimization_upper_bound = np.log(90)
                                                  
                                                  ),
@@ -108,8 +204,9 @@ me_mod_foce =  CompartmentalModel(
                                                                        ,log_transform_init_val=False
                                                                        , optimization_lower_bound=.001, 
                                                                        optimization_upper_bound=4
-                                                                       )
-                                   #ode_solver_method='BDF'
+                                                                       ),
+                                   #ode_solver_method='BDF', 
+                                   #minimize_method = 'COBYQA'
                                    )
 
 
@@ -117,9 +214,11 @@ me_mod_foce =  CompartmentalModel(
 # %%
 me_mod_foce.fit2(scale_df,checkpoint_filename=f'mod_abs_test_me_foce_{now_str}.jb', n_iters_per_checkpoint=1, parallel=False, parallel_n_jobs=4)
 scale_df['me_foce_preds'] = me_mod_foce.predict2(scale_df)
-stack_cols = ['DV', 'me_fo_preds', 'me_foce_preds']
+stack_cols = ['DV', 'DV_ln', 'DV_scale', 'me_fo_preds', 'me_foce_preds', 'me_fo_preds_alt_scale', 'me_fo_preds_ln']
 long_df = scale_df.melt(id_vars = ['SUBJID', 'TIME'], value_vars = stack_cols, value_name='Conc', var_name = 'pred_method')
 
 with open('me_mod_debug_foce.jb', 'wb') as f:
     jb.dump(me_mod_foce, f)
 
+
+# %%
