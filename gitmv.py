1dd53895030c62259087e7a96100beaebc008595
Kelley Brady <kelleyjbrady@protonmail.com>
2025-04-24 16:56:53 +0000

    Update git ignore

--- gitmv.py
+++ gitmv.py
@@ -4,11 +4,11 @@ import os
 import glob
 
 #%%
-debug_mv_files = glob.glob("/workspaces/PK-Analysis/debug_*.py")
+debug_mv_files = glob.glob("/workspaces/PK-Analysis/*scratch*")
 
 # %%
 for f in debug_mv_files:
     base = os.path.basename(f)
-    write_path = os.path.join('debug', base)
+    write_path = os.path.join('scratch', base)
     subprocess.run(['git', 'mv', f, write_path])
 # %%
