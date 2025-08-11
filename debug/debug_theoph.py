9abbfe05f16020ba683c9f425fc1ef482fcf4b73
Kelley Brady <kelleyjbrady@protonmail.com>
2025-08-11 18:12:08 +0000

    Added comparision of grad btwn jax and finitediffx

--- debug/debug_theoph.py
+++ debug/debug_theoph.py
@@ -1,7 +1,7 @@
 #%%
 
 import os
-
+import finitediffx as fdx
 # --- Control Flag ---
 USE_GPU = False
 # --------------------
@@ -207,7 +207,7 @@ if fit_model:
 #%%
 #%%
 me_mod_fo =  CompartmentalModel(
-        model_name = "debug_theoph_abs_ka-clME-vd_JAXFOCE_jaxoptspwrapLbfgsb_iftinner_nodep_omegadiag_dermal",
+        model_name = "debug_theoph_abs_ka-clME-vd_JAXFOCE_jaxoptspwrapLbfgsb_fdxouteriftinner_nodep_omegadiag_dermal",
             ode_t0_cols=[ ODEInitVals('DV'), ODEInitVals('AMT'),],
             conc_at_time_col = 'DV',
             subject_id_col = 'ID', 
@@ -274,7 +274,114 @@ if fit_model:
 if load_jb:
     me_mod_fo = jb.load(r"/workspaces/PK-Analysis/debug/logs/fitted_model_cb_debug.jb.jb")
 
-#jax.value_and_grad(me_mod_fo[0])(me_mod_fo[1])
+jvg = jax.value_and_grad(me_mod_fo[0])(me_mod_fo[1])
+
+#fvg = fdx.value_and_fgrad(me_mod_fo[0])(me_mod_fo[1])
+
+#%%
+me_mod_fo =  CompartmentalModel(
+        model_name = "debug_theoph_abs_ka-clME-vd_JAXFOCE_jaxoptspwrapLbfgsb_fdxouteriftinner_nodep_omegadiag_dermal",
+            ode_t0_cols=[ ODEInitVals('DV'), ODEInitVals('AMT'),],
+            conc_at_time_col = 'DV',
+            subject_id_col = 'ID', 
+            time_col = 'TIME',
+            population_coeff=[
+                                PopulationCoeffcient('ka', 
+                                                    optimization_init_val=1.6, 
+                                                    subject_level_intercept=True,
+                                                    #optimization_lower_bound = np.log(1e-6),
+                                                    #optimization_upper_bound = np.log(15),
+                                                    subject_level_intercept_sd_init_val = 0.6, 
+                                                    subject_level_intercept_sd_upper_bound = 20,
+                                                    subject_level_intercept_sd_lower_bound=1e-6
+                                                    ),
+                                PopulationCoeffcient('cl',
+                                                    optimization_init_val = 3,
+                                                    #optimization_lower_bound = np.log(1e-4),
+                                                    #optimization_upper_bound=np.log(25),
+                                                    subject_level_intercept=True, 
+                                                    subject_level_intercept_sd_init_val = 0.3, 
+                                                    subject_level_intercept_sd_upper_bound = 5,
+                                                    subject_level_intercept_sd_lower_bound=1e-6
+                                                    ),
+                                PopulationCoeffcient('vd', optimization_init_val = 35,
+                                                    #, optimization_lower_bound = np.log(.1)
+                                                    #,optimization_upper_bound=np.log(80), 
+                                                    subject_level_intercept=True, 
+                                                    subject_level_intercept_sd_init_val = 0.1, 
+                                                    subject_level_intercept_sd_upper_bound = 5,
+                                                    subject_level_intercept_sd_lower_bound=1e-6
+                                                    
+                                                    #, optimization_upper_bound = np.log(.05)
+                                                    ),
+                            ],
+            dep_vars= None, 
+                                    no_me_loss_function=neg2_log_likelihood_loss, 
+                                    no_me_loss_needs_sigma=True,
+
+                                    #optimizer_tol=None, 
+                                    #optimizer_tol=1e-4,
+                                    pk_model_class=OneCompartmentAbsorption, 
+                                    model_error_sigma=PopulationCoeffcient('sigma'
+                                                                            ,log_transform_init_val=False
+                                                                            , optimization_init_val=.5
+                                                                            ,optimization_lower_bound=0.00001
+                                                                            ,optimization_upper_bound=3
+                                                                            ),
+                                    #ode_solver_method='BDF'
+                                    batch_id='theoph_test1',
+                                    #minimize_method = 'COBYQA'
+                                    #use_full_omega=False, 
+                                    significant_digits=5,
+                                    me_loss_function=FO_approx_ll_loss,
+                                    jax_loss=FOCE_approx_neg2ll_loss_jax_fdxOUTER,
+                                    use_full_omega=True, 
+                                    use_surrogate_neg2ll=True, 
+                                    fit_jax_objective=True,
+                                    )
+#%%
+fit_model = True
+load_jb = False
+if fit_model:
+    me_mod_fo = me_mod_fo.fit2(df, ci_level = None, debug_fit=False, )
+if load_jb:
+    me_mod_fo = jb.load(r"/workspaces/PK-Analysis/debug/logs/fitted_model_cb_debug.jb.jb")
+
+#jvg = jax.value_and_grad(me_mod_fo[0])(me_mod_fo[1])
+
+fvg = fdx.value_and_fgrad(me_mod_fo[0])(me_mod_fo[1])
+
+
+#%%
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+labels = ["ka_pop",
+          'cl_pop',
+          "vd_pop",
+          'sigma',
+          "omega_chol_ltr00",
+          "omega_chol_ltr10",
+          "omega_chol_ltr11",
+          "omega_chol_ltr20",
+          "omega_chol_ltr21",
+          "omega_chol_ltr22",
+          
+          ]
+with sns.axes_style("whitegrid"):
+    fig, axs = plt.subplots(1,)# figsize = (10,10))
+    axs.plot(np.linspace(-350,200,5 ),np.linspace(-350,200,5 ) )
+    x = jvg[1]
+    y =  fvg[1]
+    axs.scatter(jvg[1], fvg[1] )
+    axs.set_ylabel("finitediffx.fgrad")
+    axs.set_xlabel("jax.grad")
+    for i, txt in enumerate(labels):
+        axs.annotate(txt, (x[i], y[i]), xytext=(5, 5), textcoords='offset points')
+    plt.tight_layout()
+    
+plt.savefig("prev_grad.png")
 
 #%%
 fit_pymc = True
